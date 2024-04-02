00e5abac1840c90f3db848edf29c1196aabd41dd
Wylan Neely <wylanneely@gmail.com>
2024-04-01 21:03:49 -0700

    updated InstrumentSelectView
    - added functionality to display different Instruments

--- KnowNotesPianist/Views/InstrumentSelectCell.swift
+++ KnowNotesPianist/Views/InstrumentSelectCell.swift
@@ -8,10 +8,61 @@
 import UIKit
 
 class InstrumentSelectCell: UICollectionViewCell {
-
+    
+    var instrumentID: Int = 0
+    
+    //MARK: - Outlets
+    
+    @IBOutlet weak var instrumentImage: UIImageView!
+    @IBOutlet weak var instrumentLabel: GradientLabel!
+    
+    
+    //MARK: - Setup
+        
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        setUpVariables()
+        setUpGradientColorLabel()
+    }
+    
+    func setUpVariables(){
+        let backGroundImage = returnInstrumentImage(id: self.instrumentID)
+        instrumentImage.image = backGroundImage
+        let labelText = returnLabelText(id: self.instrumentID)
+        instrumentLabel.text = labelText
+    }
+    
+    func setUpGradientColorLabel(){
+        instrumentLabel.gradientColors = [UIColor.black.cgColor, UIColor.systemPurple.cgColor]
+    }
+    
+    
+    //Images and names to be shown
+    func returnInstrumentImage(id:Int)->UIImage {
+        switch id {
+        case 0: return UIImage(named: "grandpiano")!
+        case 1: return UIImage(named: "grandpiano")!
+        case 2: return UIImage(named: "grandpiano")!
+        default:
+            return UIImage(named: "grandpiano")!
+        }
+    }
+    
+    func returnLabelText(id:Int)->String{
+        switch id {
+        case 0: return InstrumentCollectionName.FreePiano.rawValue
+        case 1: return InstrumentCollectionName.GrandPiano.rawValue
+        case 2: return InstrumentCollectionName.Keyboard.rawValue
+        default:
+            return "Piano"
+        }
     }
 
 }
+
+enum InstrumentCollectionName: String {
+    case FreePiano = "Free Piano"
+    case GrandPiano = "Grand Piano"
+    case Keyboard = "Keyboard"
+}
+
