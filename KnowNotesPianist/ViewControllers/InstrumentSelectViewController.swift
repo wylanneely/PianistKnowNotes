e10ff3e150556745e0a2697a05dafde2be70b51d
Wylan Neely <wylanneely@gmail.com>
2024-04-01 21:05:20 -0700

    updated InstrumentSelectViewController
    - collection view delegate and data source logic

--- KnowNotesPianist/ViewControllers/InstrumentSelectViewController.swift
+++ KnowNotesPianist/ViewControllers/InstrumentSelectViewController.swift
@@ -7,15 +7,42 @@
 
 import UIKit
 
-class InstrumentSelectViewController: UIViewController {
+class InstrumentSelectViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+   
+    
+    //MARK: - Overrides
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setUpCollectionView()
         // Do any additional setup after loading the view.
     }
     
-
+    //MARK: - Setup
+    func setUpCollectionView(){
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
+        self.collectionView.register(UINib(nibName: "InstrumentSelectCell", bundle: nil), forCellWithReuseIdentifier: "InstrumentSelectCell")
+    }
+    
+    //MARK: - CollectionView
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "InstrumentSelectCell", for: indexPath) as? InstrumentSelectCell else {
+            return UICollectionViewCell()
+        }
+        cell.instrumentID = indexPath.row
+        cell.awakeFromNib()
+        return cell
+    }
+    
+    //MARK: - Actions
+    
     /*
     // MARK: - Navigation
 
