ab67bfa09f5f7a7866d1faa3bf88b73541918662
Wylan Neely <wylanneely@gmail.com>
2024-04-01 19:14:32 -0700

    added InstrumentCollectionViewCell + Xib
    - created InstrumentSelectCell class and xib files

---
+++ KnowNotesPianist/Views/InstrumentSelectCell.swift
@@ -0,0 +1,17 @@
+//
+//  InstrumentSelectCell.swift
+//  KnowNotesPianist
+//
+//  Created by Wylan L Neely on 4/1/24.
+//
+
+import UIKit
+
+class InstrumentSelectCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
