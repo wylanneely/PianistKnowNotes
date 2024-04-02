89525fa2f00a047424562ca18c045ca5bd3c87f5
Wylan Neely <wylanneely@gmail.com>
2024-04-01 18:30:53 -0700

    added InstrumentSelectVC
    - created InstrumentSelectViewController class

---
+++ KnowNotesPianist/ViewControllers/InstrumentSelectViewController.swift
@@ -0,0 +1,29 @@
+//
+//  InstrumentSelectViewController.swift
+//  KnowNotesPianist
+//
+//  Created by Wylan L Neely on 4/1/24.
+//
+
+import UIKit
+
+class InstrumentSelectViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
