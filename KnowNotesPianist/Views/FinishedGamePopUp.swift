d983d45041ec77f2c91d07bb80f35b4ff16f5649
Wylan Neely <wylanneely@gmail.com>
2024-04-07 18:12:44 -0700

    created FinishedGamePopUp ViewController
    -initial class and .xib commit
    -to be used as a PopUp once game finished

---
+++ KnowNotesPianist/Views/FinishedGamePopUp.swift
@@ -0,0 +1,21 @@
+//
+//  FinishedGamePopUp.swift
+//  KnowNotesPianist
+//
+//  Created by Wylan L Neely on 4/7/24.
+//
+
+import UIKit
+
+class FinishedGamePopUp: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    
+
+}
