2e18d96523678fe3e90ec68fbc1da3dcadd632a4
Wylan Neely <wylanneely@gmail.com>
2024-04-14 16:53:57 -0700

    created InstrumentType file
    -2 enum classes for instrumentType and instrumentTypeName

---
+++ KnowNotesPianist/Models+Controllers/InstrumentType.swift
@@ -0,0 +1,24 @@
+//
+//  InstrumentType.swift
+//  KnowNotesPianist
+//
+//  Created by Wylan L Neely on 4/14/24.
+//
+
+import Foundation
+
+enum InstrumentType {
+    case FreePiano
+    case GrandPiano
+    case AcousticGuitar
+    case Keyboard
+    //case Guitar
+}
+
+
+enum InstrumentTypeName: String {
+    case FreePiano = "Free Piano"
+    case GrandPiano = "Grand Piano"
+    case AcousticGuitar = "Acoustic Guitar"
+    case Keyboard = "Keyboard"
+}
