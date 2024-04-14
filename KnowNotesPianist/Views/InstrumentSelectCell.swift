d4d0b486f49a7aeafdccfe2a28baa81f8dc1bfae
Wylan Neely <wylanneely@gmail.com>
2024-04-14 16:06:23 -0700

    updated InstrumentSelectCell class file
    -added IBAction for cellButtonTapped()
    -created and implemented InstrumentSelectDelegate into class file
    - renamed IntrumentTypeName
    -created new instrumentType "Acoustic Guitar"

--- KnowNotesPianist/Views/InstrumentSelectCell.swift
+++ KnowNotesPianist/Views/InstrumentSelectCell.swift
@@ -10,12 +10,22 @@ import UIKit
 class InstrumentSelectCell: UICollectionViewCell {
     
     var instrumentID: Int = 0
+    var delegate: InstrumentSelectDelegate?
+    
+    var InstrumentType: InstrumentTypeName {
+        switch instrumentID {
+        case 0: return InstrumentTypeName.FreePiano
+        case 1: return InstrumentTypeName.GrandPiano
+        case 2: return InstrumentTypeName.AcousticGuitar
+        case 3: return InstrumentTypeName.Keyboard
+        default: return InstrumentTypeName.FreePiano
+        }
+    }
     
     //MARK: - Outlets
     
     @IBOutlet weak var instrumentImage: UIImageView!
     @IBOutlet weak var instrumentLabel: UILabel!
-    
     
     //MARK: - Setup
         
@@ -39,31 +49,44 @@ class InstrumentSelectCell: UICollectionViewCell {
     
     
     //Images and names to be shown
-    func returnInstrumentImage(id:Int)->UIImage {
+   private func returnInstrumentImage(id:Int)->UIImage {
         switch id {
         case 0: return UIImage(named: "grandpiano")!
         case 1: return UIImage(named: "grandpiano")!
         case 2: return UIImage(named: "grandpiano")!
+        case 3: return UIImage(named: "grandpiano")!
         default:
             return UIImage(named: "grandpiano")!
         }
     }
     
-    func returnLabelText(id:Int)->String{
+   private func returnLabelText(id:Int)->String{
         switch id {
-        case 0: return InstrumentCollectionName.FreePiano.rawValue
-        case 1: return InstrumentCollectionName.GrandPiano.rawValue
-        case 2: return InstrumentCollectionName.Keyboard.rawValue
+        case 0: return InstrumentTypeName.FreePiano.rawValue
+        case 1: return InstrumentTypeName.GrandPiano.rawValue
+        case 2: return InstrumentTypeName.AcousticGuitar.rawValue
+        case 3: return InstrumentTypeName.Keyboard.rawValue
         default:
             return "Piano"
         }
     }
+    
+    //MARK: - Outlet
+    @IBAction func cellButtonTapped(_ sender: Any) {
+        delegate?.openProgressViewFor(instrument: self.InstrumentType)
+    }
+    
 
 }
 
-enum InstrumentCollectionName: String {
+protocol InstrumentSelectDelegate {
+    func openProgressViewFor(instrument:InstrumentTypeName)
+}
+
+enum InstrumentTypeName: String {
     case FreePiano = "Free Piano"
     case GrandPiano = "Grand Piano"
+    case AcousticGuitar = "Acoustic Guitar"
     case Keyboard = "Keyboard"
 }
 
