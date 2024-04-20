adca3341da56d32af95e3a4f19e1cc2d759ee9a0
Wylan Neely <wylanneely@gmail.com>
2024-04-20 13:07:40 -0700

    updated GameKitController
    - added functionality to submit scores to proper leaderboards

--- KnowNotesPianist/Models+Controllers/GameKitController.swift
+++ KnowNotesPianist/Models+Controllers/GameKitController.swift
@@ -12,16 +12,67 @@ struct GameKitController {
     
     //MARK: - Identifiers
     private let basicPianoLID = "basicPianoScore"
+    private let grandPianoLID = "grandPianoScore"
+    private let acousticGuitarLID = "acousticGuitarScore"
+    private let keyboardLID = "keyboardScore"
+    private let violinLID = "violinScore"
     
     //MARK: - Submit
     
-    func submitBasicPianoScore(score:Int) {
+    func submitLeaderboard(score: Int, instrument:InstrumentType){
+        switch instrument {
+        case .BasicPiano:
+            submitBasicPianoScore(score: score)
+        case .GrandPiano:
+            submitGrandPianoScore(score: score)
+        case .AcousticGuitar:
+            submitAcousticGuitarScore(score: score)
+        case .Keyboard:
+            submitKeyboardScore(score: score)
+        case .Violin:
+            submitViolinScore(score: score)
+        }
+    }
+    
+   private func submitBasicPianoScore(score:Int) {
         GKLeaderboard.submitScore(score, context: 0, player: GKLocalPlayer.local, leaderboardIDs: [basicPianoLID]) { error in
             if let error {
                 print(error.localizedDescription.localizedLowercase)
             }
         }
     }
+    
+    private func submitGrandPianoScore(score:Int) {
+         GKLeaderboard.submitScore(score, context: 0, player: GKLocalPlayer.local, leaderboardIDs: [grandPianoLID]) { error in
+             if let error {
+                 print(error.localizedDescription.localizedLowercase)
+             }
+         }
+     }
+    
+    private func submitAcousticGuitarScore(score:Int) {
+         GKLeaderboard.submitScore(score, context: 0, player: GKLocalPlayer.local, leaderboardIDs: [acousticGuitarLID]) { error in
+             if let error {
+                 print(error.localizedDescription.localizedLowercase)
+             }
+         }
+     }
+    
+    private func submitKeyboardScore(score:Int) {
+         GKLeaderboard.submitScore(score, context: 0, player: GKLocalPlayer.local, leaderboardIDs: [keyboardLID]) { error in
+             if let error {
+                 print(error.localizedDescription.localizedLowercase)
+             }
+         }
+     }
+    
+    private func submitViolinScore(score:Int) {
+         GKLeaderboard.submitScore(score, context: 0, player: GKLocalPlayer.local, leaderboardIDs: [violinLID]) { error in
+             if let error {
+                 print(error.localizedDescription.localizedLowercase)
+             }
+         }
+     }
     
     
     //MARK: GKAccessPoint
@@ -31,7 +82,7 @@ struct GameKitController {
         GKAccessPoint.shared.isActive = true
     }
     
-    func hiheGKAcessPoint(){
+    func hideGKAcessPoint(){
         GKAccessPoint.shared.isActive = false
     }
     
