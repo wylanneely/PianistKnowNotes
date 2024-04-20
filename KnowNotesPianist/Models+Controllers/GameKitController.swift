87bbfa902d0b78f671c5f31d6bc39ea9fc0377e0
Wylan Neely <wylanneely@gmail.com>
2024-04-19 17:17:15 -0700

    updated GameKitController
    - created functions to show() and hide() GKAccessPoint

--- KnowNotesPianist/Models+Controllers/GameKitController.swift
+++ KnowNotesPianist/Models+Controllers/GameKitController.swift
@@ -13,12 +13,26 @@ struct GameKitController {
     //MARK: - Identifiers
     private let basicPianoLID = "basicPianoScore"
     
+    //MARK: - Submit
+    
     func submitBasicPianoScore(score:Int) {
         GKLeaderboard.submitScore(score, context: 0, player: GKLocalPlayer.local, leaderboardIDs: [basicPianoLID]) { error in
             if let error {
                 print(error.localizedDescription.localizedLowercase)
             }
         }
+    }
+    
+    
+    //MARK: GKAccessPoint
+    
+    func showGKAccessPoint(){
+        GKAccessPoint.shared.location = .topLeading
+        GKAccessPoint.shared.isActive = true
+    }
+    
+    func hiheGKAcessPoint(){
+        GKAccessPoint.shared.isActive = false
     }
     
 }
