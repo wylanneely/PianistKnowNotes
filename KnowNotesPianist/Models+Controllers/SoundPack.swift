132b11282026e66589add8a8573e347a666cd9b1
Wylan Neely <wylanneely@gmail.com>
2024-05-18 15:00:30 -0700

    updated SoundPack
    -added F Note 
    -created GrandPianoSoundPack to be tested in game

--- KnowNotesPianist/Models+Controllers/SoundPack.swift
+++ KnowNotesPianist/Models+Controllers/SoundPack.swift
@@ -26,7 +26,7 @@ struct SoundPack {
 }
 
 let BasicPianoPack = SoundPack(name: "BasicPiano", type: .BasicPiano, A: FPiano_A, AS: FPiano_AS, B: FPiano_B, C: FPiano_C, CS: FPiano_CS, D: FPiano_D, DS: FPiano_DS, E: FPiano_E, F: FPiano_F, FS: FPiano_FS, G: FPiano_G, GS: FPiano_GS)
-//let GrandPianoPack = SoundPack
+let GrandPianoPack = SoundPack(name: "GrandPiano", type: .GrandPiano, A: A_GrandPiano, AS: AS_GrandPiano, B: B_GrandPiano, C: C_GrandPiano, CS: CS_GrandPiano, D: D_GrandPiano, DS: DS_GrandPiano, E: E_GrandPiano, F: F_GrandPiano, FS: FS_GrandPiano, G: G_GrandPiano, GS: GS_GrandPiano)
 
 //MARK: - Notes
 
@@ -64,7 +64,7 @@ let CS_GrandPiano = Note(note: "CS_GrandPiano", noteID: 4, soundFileName: "CS_Gr
 let D_GrandPiano = Note(note: "D_GrandPiano", noteID: 5, soundFileName: "D_GrandPiano_Test")
 let DS_GrandPiano = Note(note: "DS_GrandPiano", noteID: 6, soundFileName: "DS_GrandPiano_Test")
 let E_GrandPiano = Note(note: "E_GrandPiano", noteID: 7, soundFileName: "E_GrandPiano_Test")
-//let F_GrandPiano = Note(note: "F_GrandPiano", noteID: 8, soundFileName: "F_GrandPiano_Test")
+let F_GrandPiano = Note(note: "F_GrandPiano", noteID: 8, soundFileName: "F_GrandPiano_Test")
 let FS_GrandPiano = Note(note: "FS_GrandPiano", noteID: 9, soundFileName: "FS_GrandPiano_Test")
 let G_GrandPiano = Note(note: "G_GrandPiano", noteID: 10, soundFileName: "G_GrandPiano_Test")
 let GS_GrandPiano = Note(note: "GS_GrandPiano", noteID: 11, soundFileName: "GS_GrandPiano_Test")
