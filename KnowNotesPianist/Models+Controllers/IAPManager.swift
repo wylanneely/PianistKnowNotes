dd68c45a7c4274383e78bca4734ee1b0fd8bc138
Wylan Neely <wylanneely@gmail.com>
2024-05-27 20:10:50 -0700

    created InAppPurchaseManager file
    - class file named IAPManager

---
+++ KnowNotesPianist/Models+Controllers/IAPManager.swift
@@ -0,0 +1,13 @@
+//
+//  IAPManager.swift
+//  KnowNotesPianist
+//
+//  Created by Wylan L Neely on 5/27/24.
+//
+
+import Foundation
+import StoreKit
+
+class IAPManager : ObservableObject {
+    
+}
