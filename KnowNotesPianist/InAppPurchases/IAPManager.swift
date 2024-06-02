ce8204236b9d5cff9605614a08d087450616f181
Wylan Neely <wylanneely@gmail.com>
2024-06-01 18:26:54 -0700

    updated IAPManager

--- KnowNotesPianist/InAppPurchases/IAPManager.swift
+++ KnowNotesPianist/InAppPurchases/IAPManager.swift
@@ -9,88 +9,128 @@ import Foundation
 import StoreKit
 
 class IAPManager: NSObject, SKProductsRequestDelegate, SKPaymentTransactionObserver {
-        
+    
     var productIDS: [String] = [
-//        "grandPiano69",
-//        "acousticGuitarMajorChords",
-//        "electronicKeyboard69",
-//        "violin420"
+        //        "grandPiano69",
+        //        "acousticGuitarMajorChords",
+        //        "electronicKeyboard69",
+        //        "violin420"
         "com.wylan.apps.KnowNotes2024.piano",
         "com.wylan.apps.KnowNotes2024.guitar",
         "com.wylan.apps.KnowNotes2024.keyboard",
         "com.wylan.apps.KnowNotes2024.violin"
     ]
     
+    func getSpecificProductID(instrument: InstrumentType)-> String {
+        switch instrument {
+        case .BasicPiano:
+            return ""
+        case .GrandPiano:
+            //        "grandPiano69"
+            return "com.wylan.apps.KnowNotes2024.piano"
+        case .AcousticGuitar:
+            //        "acousticGuitarMajorChords"
+            return "com.wylan.apps.KnowNotes2024.guitar"
+        case .Keyboard:
+            //        "electronicKeyboard69"
+            return "com.wylan.apps.KnowNotes2024.keyboard"
+        case .Violin:
+            // "com.wylan.apps.KnowNotes2024.violin"
+            return  "com.wylan.apps.KnowNotes2024.violin"
+        }
+    }
+    
     
     static let shared = IAPManager()
-        private override init() {}
-
-        var productsRequest: SKProductsRequest!
-        var availableProducts = [SKProduct]()
-
-        func fetchProducts() {
-            let productIdentifiers = Set(productIDS)
-            productsRequest = SKProductsRequest(productIdentifiers: productIdentifiers)
-            productsRequest.delegate = self
-            productsRequest.start()
-           
-        }
-
-        func productsRequest(_ request: SKProductsRequest, didReceive response: SKProductsResponse) {
-            availableProducts = response.products
-            for invalidIdentifier in response.invalidProductIdentifiers {
-                print("Invalid product identifier: \(invalidIdentifier)")
-            }
-        }
-
-        func request(_ request: SKRequest, didFailWithError error: Error) {
-            print("Failed to fetch products: \(error.localizedDescription)")
-        }
-
-        func buyProduct(_ product: SKProduct) {
-            if SKPaymentQueue.canMakePayments() {
-                let payment = SKPayment(product: product)
-                SKPaymentQueue.default().add(self)
-                SKPaymentQueue.default().add(payment)
-            } else {
-                print("User cannot make payments")
-            }
-        }
-
-        func paymentQueue(_ queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction]) {
-            for transaction in transactions {
-                switch transaction.transactionState {
-                case .purchased:
-                    // Unlock content
-                    print("Purchase successful: \(transaction.payment.productIdentifier)")
-                    SKPaymentQueue.default().finishTransaction(transaction)
-                case .failed:
-                    if let error = transaction.error as NSError? {
-                        print("Transaction failed: \(error.localizedDescription)")
-                    }
-                    SKPaymentQueue.default().finishTransaction(transaction)
-                case .restored:
-                    // Restore content
-                    print("Purchase restored: \(transaction.payment.productIdentifier)")
-                    SKPaymentQueue.default().finishTransaction(transaction)
-                default:
-                    break
-                }
-            }
-        }
-
-        func paymentQueueRestoreCompletedTransactionsFinished(_ queue: SKPaymentQueue) {
-            // Handle completed restore transactions
-            print("Restore transactions finished")
-        }
-
-        func paymentQueue(_ queue: SKPaymentQueue, restoreCompletedTransactionsFailedWithError error: Error) {
-            print("Restore failed: \(error.localizedDescription)")
-        }
+    private override init() {}
     
+    var productsRequest: SKProductsRequest!
+    var availableProducts = [SKProduct]()
     
-    
+    func fetchProducts() {
+        let productIdentifiers = Set(productIDS)
+        productsRequest = SKProductsRequest(productIdentifiers: productIdentifiers)
+        productsRequest.delegate = self
+        productsRequest.start()
+        
     }
+    
+    func productsRequest(_ request: SKProductsRequest, didReceive response: SKProductsResponse) {
+        availableProducts = response.products
+        for invalidIdentifier in response.invalidProductIdentifiers {
+            print("Invalid product identifier: \(invalidIdentifier)")
+        }
+    }
+    
+    func request(_ request: SKRequest, didFailWithError error: Error) {
+        print("Failed to fetch products: \(error.localizedDescription)")
+    }
+    
+    func buyProduct(_ product: SKProduct) {
+        if SKPaymentQueue.canMakePayments() {
+            let payment = SKPayment(product: product)
+            SKPaymentQueue.default().add(self)
+            SKPaymentQueue.default().add(payment)
+        } else {
+            print("User cannot make payments")
+        }
+    }
+    
+    func paymentQueue(_ queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction]) {
+        for transaction in transactions {
+            switch transaction.transactionState {
+            case .purchased:
+                // Unlock content
+                print("Purchase successful: \(transaction.payment.productIdentifier)")
+                SKPaymentQueue.default().finishTransaction(transaction)
+            case .failed:
+                if let error = transaction.error as NSError? {
+                    print("Transaction failed: \(error.localizedDescription)")
+                }
+                SKPaymentQueue.default().finishTransaction(transaction)
+            case .restored:
+                // Restore content
+                print("Purchase restored: \(transaction.payment.productIdentifier)")
+                SKPaymentQueue.default().finishTransaction(transaction)
+            default:
+                break
+            }
+        }
+    }
+}
+//
+//    public func processReceipt() -> Bool {
+//            print("receiptValidationStarted")
+//            
+//            receipt = IAPReceipt()
+//
+//            guard receipt.isReachable,
+//                  receipt.load(),
+//                  receipt.validateSigning(),
+//                  receipt.read(),
+//                  receipt.validate() else {
+//
+//                print("receiptProcessingFailure")
+//                return false
+//            }
+//
+//            createValidatedPurchasedProductIds(receipt: receipt)
+//            print("receiptProcessingSuccess")
+//            return true
+//        }
+//
+//        func paymentQueueRestoreCompletedTransactionsFinished(_ queue: SKPaymentQueue) {
+//            // Handle completed restore transactions
+//            print("Restore transactions finished")
+//        }
+//
+//        func paymentQueue(_ queue: SKPaymentQueue, restoreCompletedTransactionsFailedWithError error: Error) {
+//            print("Restore failed: \(error.localizedDescription)")
+//        }
+//    
+//    
+//    
+      
     
 //    // Keep a strong reference to the product request.
 //    var request: SKProductsRequest!
