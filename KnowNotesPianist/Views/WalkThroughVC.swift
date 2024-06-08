610843cfe18a6a445576bd2e9ea2c272e5d34bbb
Wylan Neely <wylanneely@gmail.com>
2024-06-08 15:59:23 -0700

    created walkthroughVC class and xib file

---
+++ KnowNotesPianist/Views/WalkThroughVC.swift
@@ -0,0 +1,31 @@
+//
+//  WalkThroughVC.swift
+//  KnowNotesPianist
+//
+//  Created by Wylan L Neely on 6/8/24.
+//
+
+import UIKit
+
+class WalkThroughVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+ 
+    //MARK: Outlets and Actions
+    
+    @IBOutlet weak var hint1Label: UILabel!
+    @IBOutlet weak var hint2Label: UILabel!
+    @IBOutlet weak var okButton: UIButton!
+    
+    @IBAction func okTapped(_ sender: Any) {
+        
+        
+    }
+    
+}
